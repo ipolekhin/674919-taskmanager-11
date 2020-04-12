d7be0fdab3c9f475d822f09bf12de4ffec620d18
ipolekhin <ipolekhin@yadnex.ru>
2020-04-12 20:03:02 +0300

    3.3 Создаем моки в отдельном файле mock/filter.js

---
+++ src/mock/filter.js
@@ -0,0 +1,14 @@
+const generateFilters = () => {
+  return [{
+    'name': `all`,
+    'count': 11,
+  }, {
+    'name': `overdue`,
+    'count': 12,
+  }, {
+    'name': `today`,
+    'count': 13,
+  }];
+};
+
+export {generateFilters};
