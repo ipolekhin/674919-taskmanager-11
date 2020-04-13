f63270cc79f96b9de3dc7fe59e74cda962b8af10
ipolekhin <ipolekhin@yadnex.ru>
2020-04-13 18:48:31 +0300

    3.6.1 Добавит константы отображения задач

--- src/main.js
+++ src/main.js
@@ -8,7 +8,9 @@ import {createBoardTemplate} from "./components/board.js";
 import {generateFilters} from "./mock/filter";
 import {generateTasks} from "./mock/task";
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 22;
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 // Функция для рендеринга компонентов на страницу.
 const render = (container, template, place = `beforeend`) => {
