e69a6f50a579d2c987f63b32bbe97e50a86e7961
ipolekhin <ipolekhin@yadnex.ru>
2020-05-02 23:31:44 +0300

    4.1.7 Вынесим отрисовку задач в отдельные функции.

--- src/main.js
+++ src/main.js
@@ -10,51 +10,22 @@ import {generateFilters} from "./mock/filter";
 import {generateTasks} from "./mock/task";
 import {render, RenderPosition} from "./utils";
 
-const FIRST = 1;
 const TASK_COUNT = 20;
 const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
+const renderTask = () => {};
+const renderBoard = () => {};
+
 // Сохраняем в переменные ключевые элементы страницы.
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
 
 // Генерируем задачи
 const tasks = generateTasks(TASK_COUNT);
 const filters = generateFilters(tasks);
 
-render(siteHeaderElement, createMenuTemplate());
-render(siteMainElement, createFilterTemplate(filters));
-render(siteMainElement, createBoardTemplate());
+render(siteHeaderElement, new SiteMenuComponent().getElement());
+render(siteMainElement, new FilterComponent(filters).getElement());
 
-// Сохраняем в переменные новые ключевые элементы после рендеринга.
-const boardElement = siteMainElement.querySelector(`.board`);
-const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 
-render(boardElement, createSortingTemplate(), `afterbegin`);
-render(taskListElement, createTaskEditTemplate(tasks[0]));
-
-const createTasks = (begin, end) => {
-  const tasksShow = tasks.slice(begin, end)
-    .map((task) => createTaskTemplate(task)).join(`\n`);
-  render(taskListElement, tasksShow);
-};
-
-createTasks(FIRST, showingTasksCount);
-
-render(boardElement, createLoadMoreButtonTemplate());
-
-const loadMoreButton = document.querySelector(`.load-more`);
-
-// Создадим обработчик событий на кнопку loadmore
-loadMoreButton.addEventListener(`click`, () => {
-  const prevTaskCount = showingTasksCount;
-
-  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
-  createTasks(prevTaskCount, showingTasksCount);
-
-  if (showingTasksCount >= tasks.length) {
-    loadMoreButton.remove();
-  }
-});
