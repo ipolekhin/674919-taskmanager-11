67d3b96912a304da6b3cb01deda98f01cf178980
ipolekhin <ipolekhin@yadnex.ru>
2020-05-12 20:37:07 +0300

    6.1.1 Создадим контроллер задачи.

---
+++ src/controllers/task.js
@@ -0,0 +1,13 @@
+import TaskComponent from "../components/task";
+import TaskEditComponent from "../components/task-edit"
+import {render, replace, RenderPosition} from "../utils/render";
+
+export default class TaskController {
+  constructor(container) {
+
+  }
+
+  render(task) {
+    
+  }
+}
